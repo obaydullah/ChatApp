ea83ff6f2582dddc6eb404be4080c38914d63fe8
Obaydullah <obaydullah.2041@gmail.com>
2021-11-10 22:09:31 +0600

    hello world

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyD4B6KAVjuKVnltHRPCaGu5qP35piAPGxQ",
+    authDomain: "mern-adda.firebaseapp.com",
+    projectId: "mern-adda",
+    storageBucket: "mern-adda.appspot.com",
+    messagingSenderId: "972879536212",
+    appId: "1:972879536212:web:1a866a1eb16f59032c0dc1"
+};
+
+const app = initializeApp(firebaseConfig);
